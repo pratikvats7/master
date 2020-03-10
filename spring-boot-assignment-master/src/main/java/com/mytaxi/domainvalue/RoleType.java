99b2d12052104c799e97c269f2562fba09f0d02b
pratik <pratikfake420@gmail.com>
2020-03-10 16:25:29 +0530

    telecom/

--- spring-boot-assignment-master/src/main/java/com/mytaxi/domainvalue/RoleType.java
+++
@@ -1,5 +0,0 @@
-package com.mytaxi.domainvalue;
-
-public enum  RoleType {
-    ROLE_ADMIN, ROLE_USER, ROLE_DRIVER;
-}
--- spring-boot-assignment-master/src/main/java/com/mytaxi/domainvalue/Transmission.java
+++
@@ -1,8 +0,0 @@
-package com.mytaxi.domainvalue;
-
-public enum Transmission {
-
-    MANUAL,
-    SEMI_AUTOMATIC,
-    AUTOMATIC
-}
