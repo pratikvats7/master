9e1e2200f0049081f00d808e50f8df9b7f27b6a4
pratik <pratikfake420@gmail.com>
2020-03-10 08:17:29 +0530

    all

--- Test/spring-boot-assignment-master/src/main/java/com/mytaxi/service/manufacturer/ManufacturerService.java
+++
@@ -1,7 +0,0 @@
-package com.mytaxi.service.manufacturer;
-
-import com.mytaxi.domainobject.ManufacturerDO;
-import com.mytaxi.service.BaseService;
-
-public interface ManufacturerService extends BaseService<ManufacturerDO, Long> {
-}
--- Test/spring-boot-assignment-master/src/main/java/com/mytaxi/service/security/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.mytaxi.service.security;
-
-import com.mytaxi.domainobject.UserDO;
-import com.mytaxi.service.BaseService;
-
-public interface UserService extends BaseService<UserDO,Long> {
-    UserDO findByUserName(String userName);
-}
